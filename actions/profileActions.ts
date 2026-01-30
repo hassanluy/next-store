f85d0825f30670691ba91235d764f8d48c411ffd
hassanluy <87079724+hassanluy@users.noreply.github.com>
2026-01-31 00:32:28 +0300

    supabase with prisma configration for profile table

--- actions/profileActions.ts
+++ actions/profileActions.ts
@@ -1,12 +1,27 @@
 "use server";
+import { currentUser } from "@clerk/nextjs/server";
+import db from "../utils/db";
+import { profileSchema } from "@/utils/schema";
 // every time we use form , we will use the use server with the async function
 export async function createUserProfileAction(
   prevState: any,
   formData: FormData,
 ) {
-  "use server";
-  const firstName = formData.get("firstName");
-  const lastName = formData.get("lastName");
-  const userName = formData.get("username");
-  return { message: "Profile Created Successfully" };
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error("User not found");
+    // {key : value }
+    const rawData = Object.fromEntries(formData);
+    const validateFields = profileSchema.parse(rawData);
+    await db.profile.create({
+      data: {
+        ...validateFields,
+        clerkId: user.id,
+        profileImage: user.imageUrl ?? "",
+        email: user.emailAddresses[0]?.emailAddress ?? "",
+      },
+    });
+  } catch (error) {
+    console.log("Error creating user profile:", error);
+  }
 }
--- utils/db.ts
+++ utils/db.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@/lib/generated/prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({ adapter });
 };
 
 type PrismaClientSingleTon = ReturnType<typeof prismaClientSingleton>;
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleTon | undefined;
 };
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export default prisma;
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
